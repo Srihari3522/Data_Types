c25c55f3b03bcc56c663f171ffc0cc4052d78d4e
Srihari <sriharireddy033@gmail.com>
2024-06-09 17:59:28 +0530

    First Commit

---
+++ src/Data_Types/Data_Types_Demo.java
@@ -0,0 +1,26 @@
+package Data_Types;
+
+public class Data_Types_Demo {
+
+	public static void main(String[] args) {
+		byte b=43;
+		short s=32767;
+		int i=10;
+		long l= 345;
+		float f = 324343.00f;
+		double d= 525.5;
+		char c = 'S';
+		boolean bo = true;
+		System.out.println("The value of b is a byte data type"  + b);
+		System.out.println("The value of s is a byte data type"  + s);
+		System.out.println("The value of i is a byte data type"  + i);
+		System.out.println("The value of l is a byte data type"  + l);
+		System.out.println("The value of f is a byte data type"  + f);
+		System.out.println("The value of d is a byte data type"  + d);
+		System.out.println("The value of c is a byte data type"  + c);
+		System.out.println("The value of bo is a byte data type"  + bo);
+		
+		
+	}
+
+}
